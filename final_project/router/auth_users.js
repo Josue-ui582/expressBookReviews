77bfe57102f41ee08c547fc9e1c21570b7631f13
Josue <hounhouijosue1@gmail.com>
2026-06-01 14:58:59 +0100

    feat: create add review feature

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,12 +5,14 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username) => { //returns boolean
-  //write code to check is the username is valid
+const isValid = (username) => {
+  let usersWithSameName = users.filter((user) => user.username === username);
+  return usersWithSameName.length === 0;
 }
 
-const authenticatedUser = (username, password) => { //returns boolean
-  //write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => {
+  let validUsers = users.filter((user) => user.username === username && user.password === password);
+  return validUsers.length > 0;
 }
 
 //only registered users can login
@@ -27,14 +29,33 @@ regd_users.post("/login", (req, res) => {
     }
     return res.status(200).json({message: "login successfuly !", token : accessToken})
   }else {
-    return res.status(401).json({message : "username or password incorect"})
+    return res.status(401).json({message : "username or password incorrect"})
   }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  
+  const username = req.user.username; 
+
+  if (!review) {
+    return res.status(400).json({ message: "Le contenu de l'avis est requis" });
+  }
+
+  if (books[isbn]) {
+    books[isbn].reviews[username] = review;
+    
+    res.setHeader("Content-Type", "application/json");
+    return res.status(200).send(JSON.stringify({ 
+      message: `L'avis de l'utilisateur '${username}' pour l'ISBN ${isbn} a été enregistré avec succès.`,
+      reviews: books[isbn].reviews 
+    }, null, 4));
+
+  } else {
+    return res.status(404).json({ message: "Livre non trouvé avec cet ISBN" });
+  }
 });
 
 module.exports.authenticated = regd_users;
